98012904af5ff940244b2c4a54594663a7c11d7d
Stuart Sul <ssstuartss@gmail.com>
2025-11-28 01:43:04 +0000

    Upgrade to cluster version

--- blackwell/086-clc.cu
+++ blackwell/086-clc.cu
@@ -18,37 +18,40 @@ __global__ void init(float *data, const int N) {
     }
 }
 
-__global__ void kernel (float* data, const float alpha, const int N) {
+__global__ __cluster_dims__(2, 1, 1)
+void kernel(float* data, const float alpha, const int N) {
     __shared__ uint4 clc_handle;
     __shared__ semaphore clc_arrived;
     uint32_t phasebit = 0xFFFF0000;
 
     if (threadIdx.x == 0)
         init_semaphore(clc_arrived, 0, 1);
-    __syncthreads();
+    everyone::tma::cluster::sync();
 
+    int ctarank = cluster_ctarank();
     uint32_t curr_block_idx = blockIdx.x;
 
     while (true) {
-        if (threadIdx.x == 0) {
+        if (ctarank == 0 && threadIdx.x == 0) {
             // Prevent reordering w.r.t. CLC result write
             asm volatile("{fence.proxy.async::generic.acquire.sync_restrict::shared::cluster.cluster;}" ::: "memory");
-            
+
             // Request cancellation of a cluster not launched yet
             // Note: requesting subsequent clusterlaunchcontrol.try_cancel after a failure is undefined
             asm volatile("{clusterlaunchcontrol.try_cancel.async.shared::cta.mbarrier::complete_tx::bytes.multicast::cluster::all.b128 [%0], [%1];}"
                 :: "r"(static_cast<uint32_t>(__cvta_generic_to_shared(&clc_handle))), "r"(static_cast<uint32_t>(__cvta_generic_to_shared(&clc_arrived)))
                 : "memory"
             );
-            tma::expect_bytes(clc_arrived, sizeof(clc_handle)); // TODO change to cluster scope
         }
+        if (threadIdx.x == 0)
+            tma::cluster::expect_bytes(clc_arrived, sizeof(clc_handle), ctarank);
 
         // Do the work
         int i = curr_block_idx * blockDim.x + threadIdx.x;
         if (i < N)
             data[i] *= alpha;
 
-        wait(clc_arrived, get_phasebit<0>(phasebit, 0));
+        tma::cluster::wait(clc_arrived, get_phasebit<0>(phasebit, 0));
         update_phasebit<0>(phasebit, 0);
 
         uint32_t success;
@@ -74,10 +77,10 @@ __global__ void kernel (float* data, const float alpha, const int N) {
             break;
 
         // Prepare for next iteration
-        curr_block_idx = next_cta_id.x;
+        curr_block_idx = next_cta_id.x + ctarank; // must add cluster-local block index
 
         // Prevent next cancellation request from overwriting
-        __syncthreads(); 
+        everyone::tma::cluster::sync(); 
     }
 }
 
