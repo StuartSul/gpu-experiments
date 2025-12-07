649b7e3c7c2a385670d68fc860cbfbe3a3619e0a
Stuart Sul <ssstuartss@gmail.com>
2025-12-07 22:09:54 +0000

    Add

--- blackwell/098-l2-cache-throughput-ss.cu
+++ blackwell/098-l2-cache-throughput-ss.cu
@@ -1,5 +1,158 @@
 /*
 Per-SM throughput measurement
+
+SM_ID=147, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.95 GB/s
+SM_ID=146, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.32 us, Throughput: 121.27 GB/s
+SM_ID=145, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.38 GB/s
+SM_ID=144, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.99 GB/s
+SM_ID=143, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.25 GB/s
+SM_ID=142, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.27 GB/s
+SM_ID=129, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.70 GB/s
+SM_ID=128, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.37 GB/s
+SM_ID=127, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.20 us, Throughput: 124.89 GB/s
+SM_ID=126, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.17 GB/s
+SM_ID=123, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.14 us, Throughput: 126.55 GB/s
+SM_ID=122, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.21 GB/s
+SM_ID=115, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.22 GB/s
+SM_ID=114, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.94 GB/s
+SM_ID=113, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.12 GB/s
+SM_ID=112, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.11 GB/s
+SM_ID=109, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.96 GB/s
+SM_ID=108, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.80 GB/s
+SM_ID=101, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.92 GB/s
+SM_ID=100, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.10 GB/s
+SM_ID=099, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.14 GB/s
+SM_ID=098, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.90 GB/s
+SM_ID=095, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.17 GB/s
+SM_ID=094, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.16 GB/s
+SM_ID=087, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.68 GB/s
+SM_ID=086, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.05 GB/s
+SM_ID=085, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.98 GB/s
+SM_ID=084, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.97 GB/s
+SM_ID=081, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.26 GB/s
+SM_ID=080, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.22 GB/s
+SM_ID=073, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.32 GB/s
+SM_ID=072, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.14 GB/s
+SM_ID=071, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.91 GB/s
+SM_ID=070, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.24 GB/s
+SM_ID=067, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.22 GB/s
+SM_ID=066, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.13 GB/s
+SM_ID=065, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.14 GB/s
+SM_ID=064, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.33 us, Throughput: 121.00 GB/s
+SM_ID=057, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.94 GB/s
+SM_ID=056, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.14 us, Throughput: 126.50 GB/s
+SM_ID=055, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.19 GB/s
+SM_ID=054, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.11 GB/s
+SM_ID=051, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.88 GB/s
+SM_ID=050, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.74 GB/s
+SM_ID=049, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.24 GB/s
+SM_ID=048, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.97 GB/s
+SM_ID=041, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.18 us, Throughput: 125.38 GB/s
+SM_ID=040, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.13 GB/s
+SM_ID=039, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.06 GB/s
+SM_ID=038, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.02 GB/s
+SM_ID=035, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.93 GB/s
+SM_ID=034, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.85 GB/s
+SM_ID=033, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.35 GB/s
+SM_ID=032, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.40 GB/s
+SM_ID=025, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.20 GB/s
+SM_ID=024, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.17 us, Throughput: 125.65 GB/s
+SM_ID=023, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.06 GB/s
+SM_ID=022, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.09 GB/s
+SM_ID=019, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.23 GB/s
+SM_ID=018, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.10 GB/s
+SM_ID=017, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.27 GB/s
+SM_ID=016, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.27 GB/s
+SM_ID=009, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.03 GB/s
+SM_ID=008, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.96 GB/s
+SM_ID=007, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.35 GB/s
+SM_ID=006, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.08 GB/s
+SM_ID=003, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 125.92 GB/s
+SM_ID=002, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.16 us, Throughput: 126.18 GB/s
+SM_ID=001, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.21 GB/s
+SM_ID=000, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.15 us, Throughput: 126.41 GB/s
+
+SM_ID=135, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.65 us, Throughput: 112.85 GB/s
+SM_ID=134, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.67 us, Throughput: 112.20 GB/s
+SM_ID=133, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.76 us, Throughput: 110.16 GB/s
+SM_ID=132, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.68 us, Throughput: 111.92 GB/s
+SM_ID=131, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.92 us, Throughput: 106.58 GB/s
+SM_ID=130, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.68 us, Throughput: 111.97 GB/s
+SM_ID=079, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.68 us, Throughput: 112.14 GB/s
+SM_ID=078, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.68 us, Throughput: 111.91 GB/s
+SM_ID=077, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.91 us, Throughput: 106.76 GB/s
+SM_ID=076, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.78 us, Throughput: 109.66 GB/s
+SM_ID=075, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.06 us, Throughput: 103.55 GB/s
+SM_ID=074, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.03 us, Throughput: 104.20 GB/s
+SM_ID=069, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.67 us, Throughput: 112.23 GB/s
+SM_ID=068, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.67 us, Throughput: 112.22 GB/s
+SM_ID=047, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.99 us, Throughput: 105.07 GB/s
+SM_ID=046, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.95 us, Throughput: 105.90 GB/s
+SM_ID=037, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.87 us, Throughput: 107.56 GB/s
+SM_ID=036, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.79 us, Throughput: 109.36 GB/s
+SM_ID=005, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.80 us, Throughput: 109.13 GB/s
+SM_ID=004, N=128, REPEAT=16, Size: 0.0312 MB, Time: 4.75 us, Throughput: 110.37 GB/s
+
+
+SM_ID=141, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.22 GB/s
+SM_ID=140, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.24 us, Throughput: 100.10 GB/s
+SM_ID=139, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.09 GB/s
+SM_ID=138, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.05 GB/s
+SM_ID=137, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.15 GB/s
+SM_ID=136, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.30 GB/s
+SM_ID=125, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.15 us, Throughput: 101.83 GB/s
+SM_ID=124, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.11 us, Throughput: 102.60 GB/s
+SM_ID=121, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.17 us, Throughput: 101.44 GB/s
+SM_ID=120, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.27 GB/s
+SM_ID=119, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+SM_ID=118, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.04 GB/s
+SM_ID=117, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.14 GB/s
+SM_ID=116, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.20 us, Throughput: 100.87 GB/s
+SM_ID=111, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+SM_ID=110, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.30 GB/s
+SM_ID=107, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.01 GB/s
+SM_ID=106, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.15 GB/s
+SM_ID=105, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.15 GB/s
+SM_ID=104, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.13 GB/s
+SM_ID=103, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.28 GB/s
+SM_ID=102, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+SM_ID=097, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.30 GB/s
+SM_ID=096, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.28 GB/s
+SM_ID=093, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+SM_ID=092, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.13 GB/s
+SM_ID=091, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.30 GB/s
+SM_ID=090, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 100.99 GB/s
+SM_ID=089, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.31 GB/s
+SM_ID=088, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.20 us, Throughput: 100.91 GB/s
+SM_ID=083, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.09 GB/s
+SM_ID=082, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.17 us, Throughput: 101.34 GB/s
+SM_ID=063, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.27 GB/s
+SM_ID=062, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.26 GB/s
+SM_ID=061, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.12 GB/s
+SM_ID=060, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.12 GB/s
+SM_ID=059, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.25 GB/s
+SM_ID=058, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.25 GB/s
+SM_ID=053, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.17 us, Throughput: 101.32 GB/s
+SM_ID=052, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.25 GB/s
+SM_ID=045, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+SM_ID=044, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.14 us, Throughput: 102.09 GB/s
+SM_ID=043, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.16 us, Throughput: 101.56 GB/s
+SM_ID=042, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.13 us, Throughput: 102.13 GB/s
+SM_ID=031, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.17 us, Throughput: 101.33 GB/s
+SM_ID=030, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.02 us, Throughput: 104.42 GB/s
+SM_ID=029, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.30 GB/s
+SM_ID=028, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.26 GB/s
+SM_ID=027, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.08 GB/s
+SM_ID=026, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.17 us, Throughput: 101.32 GB/s
+SM_ID=021, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.15 GB/s
+SM_ID=020, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.31 GB/s
+SM_ID=015, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.23 us, Throughput: 100.33 GB/s
+SM_ID=014, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.06 us, Throughput: 103.57 GB/s
+SM_ID=013, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.19 us, Throughput: 101.09 GB/s
+SM_ID=012, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.13 GB/s
+SM_ID=011, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.20 GB/s
+SM_ID=010, N=128, REPEAT=16, Size: 0.0312 MB, Time: 5.18 us, Throughput: 101.29 GB/s
+
 */
 
 #include <random>
@@ -80,9 +233,7 @@ __host__ void run() {
     constexpr bool VERIFY = globals_t::VERIFY;
     constexpr int SIZE = N * N * sizeof(bf16);
 
-    printf("==================================\n");
-    printf("SM_ID=%d, N=%d, REPEAT=%d\n", SM_ID, N, REPEAT);
-    printf("Size: %.0f MB\n", SIZE / 1024. / 1024.);
+    printf("SM_ID=%03d, N=%d, REPEAT=%d, Size: %.4f MB, ", SM_ID, N, REPEAT, SIZE / 1024. / 1024.);
 
     // Sleep for 100 ms to limit power consumption and thermals
     usleep(100000);
@@ -125,8 +276,7 @@ __host__ void run() {
         cudaEventElapsedTime(&milliseconds, start, stop);
         double avg_time = double(milliseconds) / 1000.0 / num_iters;
         double bps = double(REPEAT) * SIZE / avg_time;
-        printf("Time: %.2f us\n", avg_time * 1e6);
-        printf("Throughput: %.2f GB/s\n", bps / 1e9);
+        printf("Time: %.2f us, Throughput: %.2f GB/s\n", avg_time * 1e6, bps / 1e9);
     } else {
         std::vector<bf16> h_B(N * N);
         CUDACHECK(cudaMemcpy(h_B.data(), d_B, SIZE, cudaMemcpyDeviceToHost));
@@ -157,5 +307,5 @@ __host__ int main() {
     CUDACHECK(cudaGetDeviceProperties(&prop, device_id));
     printf("L2 Size=%.2f MB\n", (double)prop.l2CacheSize / 1024 / 1024);
 
-    sm_id_loop<147, 4096, 16, 128, false>();
+    sm_id_loop<147, 128, 16, 128, false>();
 }
