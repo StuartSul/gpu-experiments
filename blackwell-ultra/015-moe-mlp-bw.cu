233e125551ab9b11b8e161ab7950f2764bb6b2db
Stuart Sul <ssstuartss@gmail.com>
2026-06-27 18:48:11 +0000

    Add

--- blackwell-ultra/015-moe-mlp-bw.cu
+++ blackwell-ultra/015-moe-mlp-bw.cu
@@ -11,14 +11,11 @@ struct config {
     static constexpr int Kb = 64;
     static constexpr int SUPERGROUP_SIZE = 8;
 
-    static constexpr bool OVERLAP_MMA_EPI = true;
     static constexpr int LOAD_PIPE_DEPTH = 5;
-    static constexpr int MMA_PIPE_DEPTH = OVERLAP_MMA_EPI ? 2 : 1;
     static constexpr int EPI_PIPE_DEPTH = 4;
-    static constexpr int CLC_PIPE_DEPTH = 1;
 
     static constexpr int CLUSTER_SIZE = 2;
-    static constexpr int NUM_CONSUMERS = OVERLAP_MMA_EPI ? 1 : 2;
+    static constexpr int NUM_CONSUMERS = 1;
     static constexpr int NUM_PRODUCERS = 1;
     static constexpr int NUM_WARPS = (NUM_CONSUMERS + NUM_PRODUCERS) * WARPGROUP_WARPS;
     static constexpr int NUM_THREADS = NUM_WARPS * WARP_THREADS;
@@ -38,161 +35,100 @@ struct globals {
     using d_gl = gl<bf16, 1, 1, -1, -1, d_tile>;
     using index_gl = gl<int, 1, 1, 1, -1>;
 
-    a_gl a;                       // (total_tokens, K)
+    a_gl a;                       // (M, K)
     b_gl b;                       // (E, N, K)
     index_gl tokens_per_expert;   // (E,)
-    d_gl d;                       // (total_tokens, N)
+    d_gl d;                       // (M, N)
 
-    __host__ __inline__ dim3 grid() const { return dim3(d.rows() / (C::NUM_CONSUMERS * C::Mb / 2) * d.cols() / C::Nb); }
+    __host__ __inline__ dim3 grid() const { return dim3(d.rows() / (C::Mb / 2) * d.cols() / C::Nb); }
 };
 
 template <typename C>
 __device__ inline void moe_mlp_kernel(const globals<C> &g) {
     using G = globals<C>;
 
-    if (threadIdx.x == 0) {
-        g.a.template prefetch_tma<typename G::a_tile>();
-        g.b.template prefetch_tma<typename G::b_tile>();
-        g.d.template prefetch_tma<typename G::d_tile>();
-    }
-
     const int cta_rank = cluster_ctarank();
+    const int cluster_idx = clusterIdx().x;
     const int iters_per_task = g.a.cols() / C::Kb;
-    const int rows_per_task = C::Mb * C::NUM_CONSUMERS;
     const int col_blocks = g.d.cols() / C::Nb;
 
     extern __shared__ int __shm[];
     tma_swizzle_allocator al((int *)&__shm[0]);
 
-    typename G::a_tile (&a_smem)[C::LOAD_PIPE_DEPTH][C::NUM_CONSUMERS] = al.allocate<G::a_tile, C::LOAD_PIPE_DEPTH, C::NUM_CONSUMERS>();
+    typename G::a_tile (&a_smem)[C::LOAD_PIPE_DEPTH] = al.allocate<G::a_tile, C::LOAD_PIPE_DEPTH>();
     typename G::b_tile (&b_smem)[C::LOAD_PIPE_DEPTH] = al.allocate<G::b_tile, C::LOAD_PIPE_DEPTH>();
-    typename G::d_tile (&d_smem)[C::NUM_CONSUMERS][C::NUM_D_TILES] = al.allocate<G::d_tile, C::NUM_CONSUMERS, C::NUM_D_TILES>();
+    typename G::d_tile (&d_smem)[C::NUM_D_TILES] = al.allocate<G::d_tile, C::NUM_D_TILES>();
 
     tensor_allocator<1, C::CLUSTER_SIZE, false> tm_alloc{};
     using d_tt_t = tt<float, C::Mb / 2, C::Nb>;
 
     __shared__ uint32_t tmem_addr;
-    __shared__ clc::handle clc_handle[C::CLC_PIPE_DEPTH];
-    __shared__ semaphore tmem_provisioned, tmem_finished, schedule_arrived[C::CLC_PIPE_DEPTH], schedule_finished[C::CLC_PIPE_DEPTH];
+    __shared__ semaphore tmem_provisioned, tmem_finished;
     __shared__ semaphore inputs_arrived[C::LOAD_PIPE_DEPTH], inputs_finished[C::LOAD_PIPE_DEPTH];
-    __shared__ semaphore outputs_arrived[C::NUM_CONSUMERS], outputs_finished[C::MMA_PIPE_DEPTH];
+    __shared__ semaphore outputs_arrived, outputs_finished;
     uint32_t bitfield = 0xFFFF0000;
 
     if (threadIdx.x == 32) {
         init_semaphore(tmem_provisioned, 0, 1);
         init_semaphore(tmem_finished, 0, 1);
         #pragma unroll
-        for (int i = 0; i < C::CLC_PIPE_DEPTH; i++) {
-            init_semaphore(schedule_arrived[i], 0, 1);
-            init_semaphore(schedule_finished[i], 0, (2 + C::NUM_CONSUMERS) * C::CLUSTER_SIZE + C::NUM_CONSUMERS);
-        }
-        #pragma unroll
         for (int i = 0; i < C::LOAD_PIPE_DEPTH; i++) {
-            init_semaphore(inputs_arrived[i], 0, C::NUM_CONSUMERS);
-            init_semaphore(inputs_finished[i], 0, C::NUM_CONSUMERS);
+            init_semaphore(inputs_arrived[i], 0, 1);
+            init_semaphore(inputs_finished[i], 0, 1);
         }
-        #pragma unroll
-        for (int i = 0; i < C::NUM_CONSUMERS; i++) {
-            init_semaphore(outputs_arrived[i], 0, 1);
-        }
-        #pragma unroll
-        for (int i = 0; i < C::MMA_PIPE_DEPTH; i++) {
-            init_semaphore(outputs_finished[i], 0, C::CLUSTER_SIZE * C::NUM_CONSUMERS);
-        }
+        init_semaphore(outputs_arrived, 0, 1);
+        init_semaphore(outputs_finished, 0, C::CLUSTER_SIZE);
     }
     everyone::tma::cluster::arrive_aligned();
 
+    int task_idx = cluster_idx;
+    int row_block_offset = 0;
+    int3 tile_coord = {-1, -1, -1};
+    for (int expert_idx = 0; expert_idx < g.b.depth(); expert_idx++) {
+        const int row_blocks = g.tokens_per_expert[{expert_idx}] / C::Mb;
+        const int num_tasks = row_blocks * col_blocks;
+        if (task_idx < num_tasks) {
+            const int2 swizzled = get_swizzled_2d_idx<C::SUPERGROUP_SIZE>(row_blocks, col_blocks, task_idx);
+            tile_coord = {row_block_offset + swizzled.x, swizzled.y, expert_idx};
+            break;
+        }
+        task_idx -= num_tasks;
+        row_block_offset += row_blocks;
+    }
+
     if (warpgroup::groupid() == C::NUM_CONSUMERS) {
-        warpgroup::decrease_registers<56>();
+        warpgroup::increase_registers<256>();
 
         if (warpgroup::warpid() == 3 && warp::elect_leader()) {
             int input_ring = 0;
-            int task_idx = blockIdx.x / C::CLUSTER_SIZE, row_block_offset = 0;
-            int3 tile = make_int3(-1, -1, -1);
-            for (int expert_idx = 0; expert_idx < g.b.depth(); expert_idx++) {
-                const int row_blocks = g.tokens_per_expert[{expert_idx}] / rows_per_task;
-                const int num_tasks = row_blocks * col_blocks;
-                if (task_idx < num_tasks) {
-                    const int2 swizzled = get_swizzled_2d_idx<C::SUPERGROUP_SIZE>(row_blocks, col_blocks, task_idx);
-                    tile = make_int3(row_block_offset + swizzled.x, swizzled.y, expert_idx);
-                    break;
-                }
-                task_idx -= num_tasks;
-                row_block_offset += row_blocks;
-            }
-            pdl::wait();
             everyone::tma::cluster::wait();
-            for (int task_iter = 0; true; task_iter++) {
-                for (int idx = 0; idx < iters_per_task; idx++) {
-                    wait(inputs_finished[input_ring], get_phasebit<1>(bitfield, input_ring));
-                    #pragma unroll
-                    for (int i = 0; i < C::NUM_CONSUMERS; i++)
-                        tma::cluster::load_async(a_smem[input_ring][i], g.a, {(tile.x * 2 + cta_rank) * C::NUM_CONSUMERS + i, idx}, inputs_arrived[input_ring], (uint16_t)(1 << cta_rank), 0);
-                    tma::cluster::load_async(b_smem[input_ring], g.b, {tile.z, tile.y * 2 + cta_rank, idx}, inputs_arrived[input_ring], (uint16_t)(1 << cta_rank), 0);
-                    update_phasebit<1>(bitfield, input_ring);
-                    input_ring = ring_advance<C::LOAD_PIPE_DEPTH>(input_ring);
-                }
-                wait(schedule_arrived[task_iter % C::CLC_PIPE_DEPTH], (task_iter / C::CLC_PIPE_DEPTH) % 2);
-                auto schedule = clc::query(clc_handle[task_iter % C::CLC_PIPE_DEPTH]);
-                tma::cluster::arrive(schedule_finished[task_iter % C::CLC_PIPE_DEPTH], 0);
-                if (schedule.success) {
-                    task_idx = schedule.x / C::CLUSTER_SIZE;
-                    row_block_offset = 0;
-                    for (int expert_idx = 0; expert_idx < g.b.depth(); expert_idx++) {
-                        const int row_blocks = g.tokens_per_expert[{expert_idx}] / rows_per_task;
-                        const int num_tasks = row_blocks * col_blocks;
-                        if (task_idx < num_tasks) {
-                            const int2 swizzled = get_swizzled_2d_idx<C::SUPERGROUP_SIZE>(row_blocks, col_blocks, task_idx);
-                            tile = make_int3(row_block_offset + swizzled.x, swizzled.y, expert_idx);
-                            break;
-                        }
-                        task_idx -= num_tasks;
-                        row_block_offset += row_blocks;
-                    }
-                } else break;
+            for (int idx = 0; idx < iters_per_task; idx++) {
+                wait(inputs_finished[input_ring], get_phasebit<1>(bitfield, input_ring));
+                tma::cluster::load_async(a_smem[input_ring], g.a, {tile_coord.x * 2 + cta_rank, idx}, inputs_arrived[input_ring], (uint16_t)(1 << cta_rank), 0);
+                tma::cluster::load_async(b_smem[input_ring], g.b, {tile_coord.z, tile_coord.y * 2 + cta_rank, idx}, inputs_arrived[input_ring], (uint16_t)(1 << cta_rank), 0);
+                update_phasebit<1>(bitfield, input_ring);
+                input_ring = ring_advance<C::LOAD_PIPE_DEPTH>(input_ring);
             }
-        } else if (warpgroup::warpid() == 2 && warp::elect_leader()) {
-            everyone::tma::cluster::wait();
-            for (int task_iter = 0; true; task_iter++) {
-                if (cta_rank == 0) {
-                    wait(schedule_finished[task_iter % C::CLC_PIPE_DEPTH], ((task_iter + C::CLC_PIPE_DEPTH) / C::CLC_PIPE_DEPTH) % 2);
-                    clc::schedule(clc_handle[task_iter % C::CLC_PIPE_DEPTH], schedule_arrived[task_iter % C::CLC_PIPE_DEPTH]);
-                }
-                tma::expect_bytes(schedule_arrived[task_iter % C::CLC_PIPE_DEPTH], sizeof(clc_handle[task_iter % C::CLC_PIPE_DEPTH]));
-                wait(schedule_arrived[task_iter % C::CLC_PIPE_DEPTH], (task_iter / C::CLC_PIPE_DEPTH) % 2);
-                auto schedule = clc::query(clc_handle[task_iter % C::CLC_PIPE_DEPTH]);
-                tma::cluster::arrive(schedule_finished[task_iter % C::CLC_PIPE_DEPTH], 0);
-                if (!schedule.success) break;
-            }
-        } else if (cta_rank == 0 && warpgroup::warpid() < C::NUM_CONSUMERS && warp::elect_leader()) {
+        } else if (cta_rank == 0 && warpgroup::warpid() == 0 && warp::elect_leader()) {
             everyone::tma::cluster::wait();
             wait(tmem_provisioned, 0);
             tm_alloc.set_addr(tmem_addr);
-            d_tt_t d_tt[C::MMA_PIPE_DEPTH];
-            #pragma unroll
-            for (int i = 0; i < C::MMA_PIPE_DEPTH; i++)
-                d_tt[i] = tm_alloc.template allocate<d_tt_t>((i + warpgroup::warpid()) * C::Nb);
+            d_tt_t d_tt = tm_alloc.template allocate<d_tt_t>(0);
             int input_ring = 0;
-            for (int task_iter = 0; true; task_iter++) {
-                wait(schedule_arrived[task_iter % C::CLC_PIPE_DEPTH], (task_iter / C::CLC_PIPE_DEPTH) % 2);
-                auto schedule = clc::query(clc_handle[task_iter % C::CLC_PIPE_DEPTH]);
-                tma::cluster::arrive(schedule_finished[task_iter % C::CLC_PIPE_DEPTH], 0);
-                wait(outputs_finished[task_iter % C::MMA_PIPE_DEPTH], ((task_iter + C::MMA_PIPE_DEPTH) / C::MMA_PIPE_DEPTH) % 2);
-                for (int idx = 0; idx < iters_per_task; idx++) {
-                    tma::expect_bytes(inputs_arrived[input_ring], (C::CLUSTER_SIZE * C::NUM_CONSUMERS * sizeof(G::a_tile) + 2 * sizeof(G::b_tile)) / C::NUM_CONSUMERS);
-                    wait(inputs_arrived[input_ring], get_phasebit<0>(bitfield, input_ring));
-                    if (idx == 0) mm2_ABt(d_tt[task_iter % C::MMA_PIPE_DEPTH], a_smem[input_ring][warpgroup::warpid()], b_smem[input_ring], inputs_finished[input_ring]);
-                    else mma2_ABt(d_tt[task_iter % C::MMA_PIPE_DEPTH], a_smem[input_ring][warpgroup::warpid()], b_smem[input_ring], inputs_finished[input_ring]);
-                    update_phasebit<0>(bitfield, input_ring);
-                    input_ring = ring_advance<C::LOAD_PIPE_DEPTH>(input_ring);
-                }
-                detail::tcgen05::commit<C::CLUSTER_SIZE>(outputs_arrived[warpgroup::warpid()]);
-                if (!schedule.success) break;
+            wait(outputs_finished, 1); // noop, left here for future expansion
+            for (int idx = 0; idx < iters_per_task; idx++) {
+                tma::expect_bytes(inputs_arrived[input_ring], C::CLUSTER_SIZE * sizeof(G::a_tile) + 2 * sizeof(G::b_tile));
+                wait(inputs_arrived[input_ring], get_phasebit<0>(bitfield, input_ring));
+                if (idx == 0) mm2_ABt (d_tt, a_smem[input_ring], b_smem[input_ring], inputs_finished[input_ring]);
+                else          mma2_ABt(d_tt, a_smem[input_ring], b_smem[input_ring], inputs_finished[input_ring]);
+                update_phasebit<0>(bitfield, input_ring);
+                input_ring = ring_advance<C::LOAD_PIPE_DEPTH>(input_ring);
             }
+            detail::tcgen05::commit<C::CLUSTER_SIZE>(outputs_arrived);
         }
     } else {
-        using epilogue_group = group<WARPGROUP_WARPS * C::NUM_CONSUMERS>;
-        if constexpr (!C::OVERLAP_MMA_EPI) warpgroup::increase_registers<224>();
+        using epilogue_group = group<WARPGROUP_WARPS>;
+        warpgroup::increase_registers<256>();
         everyone::tma::cluster::wait_aligned();
         if (epilogue_group::warpid() == 0) {
             tm_alloc.provision(tmem_addr);
@@ -200,82 +136,22 @@ __device__ inline void moe_mlp_kernel(const globals<C> &g) {
         }
         wait(tmem_provisioned, 0);
         tm_alloc.set_addr(tmem_addr);
-        d_tt_t d_tt[C::MMA_PIPE_DEPTH];
+        d_tt_t d_tt = tm_alloc.template allocate<d_tt_t>(0);
+        wait(outputs_arrived, 0);
+        rt_bf<C::Mb / 8, C::Nb / C::EPI_PIPE_DEPTH> d_reg[C::EPI_PIPE_DEPTH];
         #pragma unroll
-        for (int i = 0; i < C::MMA_PIPE_DEPTH; i++)
-            d_tt[i] = tm_alloc.template allocate<d_tt_t>((i + warpgroup::groupid()) * C::Nb);
-        int3 tile;
-        int task_idx = blockIdx.x / C::CLUSTER_SIZE, row_block_offset = 0;
-        int3 next_tile = make_int3(-1, -1, -1);
-        for (int expert_idx = 0; expert_idx < g.b.depth(); expert_idx++) {
-            const int row_blocks = g.tokens_per_expert[{expert_idx}] / rows_per_task;
-            const int num_tasks = row_blocks * col_blocks;
-            if (task_idx < num_tasks) {
-                const int2 swizzled = get_swizzled_2d_idx<C::SUPERGROUP_SIZE>(row_blocks, col_blocks, task_idx);
-                next_tile = make_int3(row_block_offset + swizzled.x, swizzled.y, expert_idx);
-                break;
-            }
-            task_idx -= num_tasks;
-            row_block_offset += row_blocks;
-        }
-        for (int task_iter = 0; true; task_iter++) {
-            tile = next_tile;
-            wait(schedule_arrived[task_iter % C::CLC_PIPE_DEPTH], (task_iter / C::CLC_PIPE_DEPTH) % 2);
-            auto schedule = clc::query(clc_handle[task_iter % C::CLC_PIPE_DEPTH]);
-            warpgroup::sync(warpgroup::groupid() + 1);
-            warpgroup::tma::cluster::arrive(schedule_finished[task_iter % C::CLC_PIPE_DEPTH], 0);
-            if (schedule.success) {
-                task_idx = schedule.x / C::CLUSTER_SIZE;
-                row_block_offset = 0;
-                for (int expert_idx = 0; expert_idx < g.b.depth(); expert_idx++) {
-                    const int row_blocks = g.tokens_per_expert[{expert_idx}] / rows_per_task;
-                    const int num_tasks = row_blocks * col_blocks;
-                    if (task_idx < num_tasks) {
-                        const int2 swizzled = get_swizzled_2d_idx<C::SUPERGROUP_SIZE>(row_blocks, col_blocks, task_idx);
-                        next_tile = make_int3(row_block_offset + swizzled.x, swizzled.y, expert_idx);
-                        break;
-                    }
-                    task_idx -= num_tasks;
-                    row_block_offset += row_blocks;
-                }
-            }
-            wait(outputs_arrived[warpgroup::groupid()], task_iter % 2);
-            if constexpr (C::OVERLAP_MMA_EPI) {
-                rt_bf<C::Mb / 8, C::Nb / C::EPI_PIPE_DEPTH> d_reg;
-                #pragma unroll
-                for (int i = 0; i < C::EPI_PIPE_DEPTH; i++) {
-                    warpgroup::load_async(d_reg, d_tt[task_iter % C::MMA_PIPE_DEPTH].template subtile<tt<float, C::Mb / 2, C::Nb / C::EPI_PIPE_DEPTH>>(0, C::Nb / C::EPI_PIPE_DEPTH * i));
-                    if (i == C::EPI_PIPE_DEPTH - 1) {
-                        tensor_load_wait();
-                        warpgroup::sync(warpgroup::groupid() + 1);
-                        if (!schedule.success) warpgroup::pdl::arrive();
-                        warpgroup::tma::cluster::arrive(outputs_finished[task_iter % C::MMA_PIPE_DEPTH], 0);
-                    }
-                    warpgroup::tma::store_async_read_wait<C::NUM_D_TILES - 1>();
-                    warpgroup::sync(warpgroup::groupid() + 1);
-                    warpgroup::store(d_smem[warpgroup::groupid()][i % C::NUM_D_TILES], d_reg);
-                    warpgroup::sync(warpgroup::groupid() + 1);
-                    warpgroup::tma::store_async<dim::ROW, cache_policy::EVICT_FIRST>(g.d, d_smem[warpgroup::groupid()][i % C::NUM_D_TILES], {(2 * tile.x + cta_rank) * C::NUM_CONSUMERS + warpgroup::groupid(), C::EPI_PIPE_DEPTH * tile.y + i});
-                }
-            } else {
-                rt_bf<C::Mb / 8, C::Nb / C::EPI_PIPE_DEPTH> d_reg[C::EPI_PIPE_DEPTH];
-                #pragma unroll
-                for (int i = 0; i < C::EPI_PIPE_DEPTH; i++)
-                    warpgroup::load_async(d_reg[i], d_tt[task_iter % C::MMA_PIPE_DEPTH].template subtile<tt<float, C::Mb / 2, C::Nb / C::EPI_PIPE_DEPTH>>(0, C::Nb / C::EPI_PIPE_DEPTH * i));
-                tensor_load_wait();
-                warpgroup::sync(warpgroup::groupid() + 1);
-                if (!schedule.success) warpgroup::pdl::arrive();
-                warpgroup::tma::cluster::arrive(outputs_finished[task_iter % C::MMA_PIPE_DEPTH], 0);
-                #pragma unroll
-                for (int i = 0; i < C::EPI_PIPE_DEPTH; i++) {
-                    warpgroup::tma::store_async_read_wait<C::NUM_D_TILES - 1>();
-                    warpgroup::sync(warpgroup::groupid() + 1);
-                    warpgroup::store(d_smem[warpgroup::groupid()][i % C::NUM_D_TILES], d_reg[i]);
-                    warpgroup::sync(warpgroup::groupid() + 1);
-                    warpgroup::tma::store_async<dim::ROW, cache_policy::EVICT_FIRST>(g.d, d_smem[warpgroup::groupid()][i % C::NUM_D_TILES], {(2 * tile.x + cta_rank) * C::NUM_CONSUMERS + warpgroup::groupid(), C::EPI_PIPE_DEPTH * tile.y + i});
-                }
-            }
-            if (!schedule.success) break;
+        for (int i = 0; i < C::EPI_PIPE_DEPTH; i++)
+            warpgroup::load_async(d_reg[i], d_tt.template subtile<tt<float, C::Mb / 2, C::Nb / C::EPI_PIPE_DEPTH>>(0, C::Nb / C::EPI_PIPE_DEPTH * i));
+        tensor_load_wait();
+        warpgroup::sync(1);
+        warpgroup::tma::cluster::arrive(outputs_finished, 0);
+        #pragma unroll
+        for (int i = 0; i < C::EPI_PIPE_DEPTH; i++) {
+            warpgroup::tma::store_async_read_wait<C::NUM_D_TILES - 1>();
+            warpgroup::sync(1);
+            warpgroup::store(d_smem[i % C::NUM_D_TILES], d_reg[i]);
+            warpgroup::sync(1);
+            warpgroup::tma::store_async<dim::ROW, cache_policy::EVICT_FIRST>(g.d, d_smem[i % C::NUM_D_TILES], {2 * tile_coord.x + cta_rank, C::EPI_PIPE_DEPTH * tile_coord.y + i});
         }
         epilogue_group::sync(4);
         if (epilogue_group::warpid() == 0) {
